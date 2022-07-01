860e50d47904e1dae524d5d9c271ba9b5ce3eb66
Mike Shippy <mikeship04@gmail.com>
2022-07-01 13:06:09 -0700

    commit tree 256575fa5c22706c5e9b7a561cb2b24e2922dc5c

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining-1)
+    }, 1000)
+    if (timeRemaining === 0) {
+      setTimeRemaining(parseInt(10))
+      onAnswered(false)
+    }
+    return () => clearTimeout(timer)
+  }, [timeRemaining])
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
